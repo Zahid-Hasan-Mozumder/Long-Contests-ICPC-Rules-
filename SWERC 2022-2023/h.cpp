99ec7bb906d270704808914b58bb5660902ecbe5
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-02-21 11:22:32 +0600

    Create h.cpp

---
+++ SWERC 2022-2023/h.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <climits>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vvpli vector<vector<pair<ll, int>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define si stack<int>
+#define sll stack<ll>
+#define qi queue<int>
+#define qll queue<ll>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+
+int main(){
+
+    zahid();
+    
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vi a(n), b(n);
+        map<int, int> mp;
+        int cnt = 1;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+            mp[a[i]] = i;
+        }
+        for(int i=0; i<n; i++){
+            cin>>b[i];
+        }
+        for(int i=1; i<n; i++){
+            if(mp[b[i]] > mp[b[i-1]])
+                cnt++;
+            else
+                cnt = 1;
+        }
+        cout<< n - cnt << '\n';
+    }
+}
